3170e0642b9172fe1e9f474d4657a3c71853ba66
MingyuanXu <ming_yxu@163.com>
2019-12-21 13:58:32 +0800

    remove esoinn_train model

--- ESOI_HDNN_MD/Train/__init__.py
+++ ESOI_HDNN_MD/Train/__init__.py
@@ -1,10 +1,8 @@
 #!/usr/bin/env python
 # coding=utf-8
-#from .LoadNN import *
 from .Productor import *
 from .Consumer import *
 from .Esoinner import *
 from .Trainer import * 
 from .Dataer import *
-from .Esoinn_train import *
 from .Jobqueue import * 
