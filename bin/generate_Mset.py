a884604da694b125c35db206b097f8402ca04ada
MingyuanXu <ming_yxu@163.com>
2019-12-23 17:25:27 +0800

    add RESP MSet generation module in bin

--- bin/generate_Mset.py
+++ bin/generate_Mset.py
@@ -1,9 +1,9 @@
-from multiprocessing import Queue,Process,Manager
 from TensorMol import  *
 from ESOI_HDNN_MD import *
 from ESOI_HDNN_MD.Base import *
 import os
 import math
+import multiprocessing 
 TMPset=MSet('2l30_resp')
 pathlist=['./']
 def get_filelist(path,keyword):
@@ -13,13 +13,27 @@ def get_filelist(path,keyword):
             if keyword in filename:
                 filelist.append([home,filename,os.path.join(home,filename)])
     return filelist 
-loglist=[]
-for path in pathlist:
-    loglist+=get_filelist(path,'.log')
-print (loglist[0])
-for logpath,filename,logfile in loglist[:1]:
+def get_resp_mol(para):
+    logpath,filename,logfile=para
     mol=Molnew(name=filename.strip('.log'))
     mol.Update_from_Gaulog(logpath+'/')
     mol.CalculateAtomization('HF/6-31g*')
-    print ()
+    flag,respc=cal_resp_charge(logfile)
+    if flag==True:
+        mol.properties['resp_charge']=respc
+        return [flag,mol]
+    else:
+        print ("RESP of %s is failed!"%mol.name)
+        return [flag,None]
+pool=multiprocessing.Pool(processes=4)
 
+loglist=[]
+for path in pathlist:
+    loglist+=get_filelist(path,'.log')
+mollist=pool.imap(get_resp_mol,loglist[:8])
+#print (mollist)
+for result in mollist:
+    if result[0]==True:
+        TMPset.mols.append (result[1])
+TMPset.Save()
+        
